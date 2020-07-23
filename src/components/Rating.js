87120c8d07d90f106e6f36c67d565541b7b6ebe5
antonio <antoniopresto@gmail.com>
2020-07-23 11:57:51 -0300

    feat: Add Rating component

---
+++ src/components/Rating.js
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import cx from 'classnames'
+
+const RatingWrapper = styled.div`
+  width: 100%;
+
+  .star {
+    opacity: 0.4;
+    &.active {
+      opacity: 1;
+    }
+
+    margin-right: 5px;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+`
+
+const Star = ({ active }) => <span className={cx('star', { active })}>★</span>
+
+export const Rating = props => {
+  const { className, rating = 0 } = props
+
+  const stars = React.useMemo(() => {
+    const result = []
+
+    for (let i = 0; i < 5; i++) {
+      result.push(<Star active={i < rating} key={i} />)
+    }
+
+    return result
+  }, [rating])
+
+  return (
+    <RatingWrapper className={cx(className, 'Rating Rating_Wrapper')}>
+      {stars}
+    </RatingWrapper>
+  )
+}
